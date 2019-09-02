2b33136dcc2e6ccbf77818cc5906629bc3911542
Steve Rumizen <steverumizen@gmail.com>
2019-09-02 16:08:39 -0600

    Add login inputs and handlers

---
+++ HomeScreen.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { View, Text, Button, ImageBackground } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+export default class HomeScreen extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    displayLogin: false
+  };
+
+  handleEmail = text => {
+    this.setState({ email: text });
+  };
+
+  handlePassword = text => {
+    this.setState({ password: text });
+  };
+
+  render() {
+    return (
+      <ImageBackground
+        source={require("./assets/tools.jpg")}
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex"
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            marginBottom: 200
+          }}
+        >
+          <Text style={{ fontSize: 62, fontWeight: "900" }}>FixUp</Text>
+          <View>
+            <TextInput
+              style={{
+                width: 200,
+                height: 40,
+                borderColor: "black",
+                borderWidth: 1,
+                borderRadius: 4,
+                padding: 5,
+                marginBottom: 10
+              }}
+              name="email"
+              onChangeText={this.handleEmail}
+              value={this.state.email}
+              placeholder="Enter email"
+            />
+            <TextInput
+              style={{
+                width: 200,
+                height: 40,
+                borderColor: "black",
+                borderWidth: 1,
+                borderRadius: 4,
+                padding: 5,
+              }}
+              name="password"
+              onChangeText={this.handlePassword}
+              value={this.state.password}
+              placeholder="Enter password"
+            />
+          </View>
+          <Button
+            title="Login"
+            onPress={() => this.props.navigation.navigate("Details")}
+          ></Button>
+          <Text>or</Text>
+          <Button
+            title="Sign Up"
+            onPress={() => this.props.navigation.navigate("Details")}
+          ></Button>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
