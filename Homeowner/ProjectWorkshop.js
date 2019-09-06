568b0d2a53d39afacb0be599852d36da33092af0
Antonio Fry <antoniofry2000@gmail.com>
2019-09-06 12:23:02 -0600

    style ProjectWorkshop

--- Homeowner/ProjectWorkshop.js
+++ Homeowner/ProjectWorkshop.js
@@ -10,14 +10,29 @@ export default class ProjectWorkshop extends Component {
   }
 
   render() {
-    <View>
-      <TouchableOpacity>
-        <Text>Post New Project</Text>
-      </TouchableOpacity>
-    </View>
+    return (
+      <View style={styles.mainContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={style.text}>Post a New Project</Text>
+        </TouchableOpacity>
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  
+  mainContainer: {
+    justifyContent: "center",
+    alignItems: 'center',
+    height: 50,
+    width: "50%"
+  },
+  button: {
+    justifyContent: 'center',
+    backgroundColor: "#7C9EB2",
+    borderRadius: 5
+  },
+  text: {
+    fontSize: 20
+  }
 })
