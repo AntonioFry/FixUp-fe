0ec86cb170ef2676e5abd913b8e120ecf684804c
Antonio Fry <antoniofry2000@gmail.com>
2019-09-05 22:12:41 -0600

    setup basic layout for project wrokshop page

---
+++ Homeowner/ProjectWorkshop.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+
+export default class ProjectWorkshop extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    <View>
+      <TouchableOpacity>
+        <Text>Post New Project</Text>
+      </TouchableOpacity>
+    </View>
+  }
+}
+
+const styles = StyleSheet.create({
+
+})
