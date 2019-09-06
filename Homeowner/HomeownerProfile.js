e9d6bac736a0f6b760122a2588a1e78532b4a14e
Antonio Fry <antoniofry2000@gmail.com>
2019-09-05 20:51:53 -0600

    style homeowner profile page

--- Homeowner/HomeownerProfile.js
+++ Homeowner/HomeownerProfile.js
@@ -14,15 +14,14 @@ export default class HomeownerProfile extends Component {
     const userInfo = Object.keys(data).map(info => {
       return (
         <View style={styles.infoContainer}>
-          <Text style={styles.textLine}>
-            <Text style={styles.allText}>{info.split("_").join(" ")}: </Text>
-            <Text style={styles.allText}>{data[info]}</Text>
-          </Text>
+          <Text style={styles.allText}>{info.split("_").join(" ")}: {data[info]}</Text>
         </View>
       )
     })
+    userInfo.pop();
     return  (
       <View style={styles.mainContainer}>
+        <Text style={styles.pageLabel}>Account Information</Text>
         {userInfo}
       </View>
     )
@@ -31,23 +30,27 @@ export default class HomeownerProfile extends Component {
 
 const styles = StyleSheet.create({
   infoContainer: {
+    justifyContent: "center",
     borderStyle: 'solid',
     borderWidth: 1,
     borderColor: "#CBC5C9",
     width: "100%",
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
+    height: 40
   },
   allText: {
-    fontSize: 20,
+    fontSize: 22,
     color: "#2D2C2D",
-  },
-  textLine: {
-    flexDirection: "row",
-    justifyContent: "space-between",
   },
   mainContainer: {
     paddingVertical: 50,
     height: "100%",
     width: "100%",
+  },
+  pageLabel: {
+    marginLeft: 5,
+    marginBottom: 5,
+    fontSize: 20,
+    fontWeight: "bold"
   }
 })
