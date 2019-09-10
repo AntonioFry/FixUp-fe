244c25bc4b05d02038708d20d98435271fbf4243
Antonio Fry <antoniofry2000@gmail.com>
2019-09-10 14:40:26 -0600

    initialize contractorPage component

---
+++ Homeowner/ContractorPage/ContractorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from "react-native";
+import ProfileCategory from "./ProfileCategory";
+
+export const ContractorPage = () => {
+  
+  return (
+    <View>
+      
+    </View>
+  )
+}
