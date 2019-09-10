50bf1371a662c4b88ac2a8d4982c7927f7f28f68
Antonio Fry <antoniofry2000@gmail.com>
2019-09-10 16:25:54 -0600

    style contractor page bars

--- Homeowner/ContractorPage/ContractorPage.js
+++ Homeowner/ContractorPage/ContractorPage.js
@@ -5,7 +5,7 @@ import ProfileCategory from "./ProfileCategory";
 const ContractorPage = (props) => {
   const { name, email, zip, phone_number, category } = props.navigation.state.params;
   return (
-    <View>
+    <View style={styles.container}>
       <ProfileCategory category={"Name"} value={name} />
       <ProfileCategory category={"Email"} value={email} />
       <ProfileCategory category={"Zip Code"} value={zip} />
@@ -16,7 +16,11 @@ const ContractorPage = (props) => {
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    paddingTop: 25,
+    justifyContent: "flex-start",
+    width: "100%"
+  },
 })
 
 export default ContractorPage;
--- Homeowner/ContractorPage/ProfileCategory.js
+++ Homeowner/ContractorPage/ProfileCategory.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default ProfileCategory = ({ category, value }) => {
   console.log(value)
   return (
-    <View>
-      <Text>{category}</Text>
-      <View>
-        <Text>{value}</Text>
+    <View style={styles.categoryWrapper}>
+      <Text style={styles.leftText}>{category}</Text>
+      <View style={styles.rightTextWrapper}>
+        <Text style={styles.rightText}>{value}</Text>
       </View>
     </View> 
   )
 }
+
+const styles = StyleSheet.create({
+  leftText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "700"
+  },
+  rightText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "100"
+  },
+  rightTextWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  categoryWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#7C9EB2",
+    height: 70,
+    paddingHorizontal: 10,
+    borderColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "solid"
+  },
+})
