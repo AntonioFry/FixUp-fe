df6a1326117cb48e0b6373538ed375f45a61608b
Antonio Fry <antoniofry2000@gmail.com>
2019-09-10 20:34:00 -0600

    setup test for ContractorPage

---
+++ Homeowner/ContractorPage/ContractorPage.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ContractorPage from './ContractorPage';
+
+describe('ContractorPage', () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      navigation: {
+        state: {
+          params: {
+            name: "",
+            email: "",
+            zip: "",
+            phone_number: "",
+            category: ""
+          }
+        }
+      },
+      wrapper = Shallow(<ContractorPage props={props} />)
+    }
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+})
