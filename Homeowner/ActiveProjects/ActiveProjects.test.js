c5ed618ef3acc5e7a8ebaff9680673727a891654
Antonio Fry <antoniofry2000@gmail.com>
2019-09-10 20:00:55 -0600

    setup test for ActiveProjects

---
+++ Homeowner/ActiveProjects/ActiveProjects.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Shallow } from 'enzyme';
+import ActiveProjects from './ActiveProjects';
+
+describe('ActiveProjects', () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      navigation: {
+        navigate: jest.fn(),
+        getParam: jest.fn()
+      },
+      wrapper = Shallow(<ActiveProjects props={props}/>) 
+    }
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+})
