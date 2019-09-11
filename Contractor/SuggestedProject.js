f6a7a2efb65f2b612c9b10fa39f6e779590c9d28
Steve Rumizen <steverumizen@gmail.com>
2019-09-11 17:51:54 -0600

    Restyle suggested projects with bigger image and ability to click to view full

--- Contractor/SuggestedProject.js
+++ Contractor/SuggestedProject.js
@@ -2,17 +2,33 @@ import React from "react"
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 
 export default class SuggestedProject extends React.Component {
+  goToProject = () => {
+    const {
+      title,
+      description,
+      photo,
+      navigation,
+      category
+    } = this.props;
+    navigation.navigate("ViewProject", {
+      title,
+      description,
+      photo,
+      category,
+    });
+  }
+
   render() {
-    const { title, description, image, category, index } = this.props;
-    const base64image = `data:image/jpg;base64,${image}`;
+    const { title, photo, index } = this.props;
+    const base64image = `data:image/jpg;base64,${photo}`;
     return (
       <View style={styles.suggestedProjectWrapper} key={index}>
         <Text style={styles.projTitle}>{title}</Text>
-        <Image
-          style={styles.projPhoto}
-          source={{ uri: base64image }}
-        />
-        <Text>{description}</Text>
+        <Image style={styles.projPhoto} source={{ uri: base64image }} />
+        <TouchableOpacity onPress={this.goToProject} style={styles.viewButton}>
+          <Text style={styles.seeMore}>See more</Text>
+          <Image style={styles.moreIcon} source={require("../assets/more.png")} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -39,11 +55,24 @@ const styles = StyleSheet.create({
     elevation: 2
   },
   projPhoto: {
-    height: 180,
+    height: 220,
     width: 230,
     borderRadius: 4
   },
   projTitle: {
     fontWeight: "700"
+  },
+  viewButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  moreIcon: {
+    height: 15,
+    width: 15
+  },
+  seeMore: {
+    fontWeight: "500",
+    marginRight: 7
   }
 });
