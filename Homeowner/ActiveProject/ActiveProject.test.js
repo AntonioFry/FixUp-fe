3d1f0b66bdafaa3166d0b7fefa2acf3b2548054d
Antonio Fry <antoniofry2000@gmail.com>
2019-09-10 19:35:23 -0600

    setup test for ActiveProject component

--- Homeowner/ActiveProject/ActiveProject.test.js
+++ Homeowner/ActiveProject/ActiveProject.test.js
@@ -1,2 +1,25 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import react from 'react';
+import { shallow } from 'enzyme';
+import ActiveProject from './ActiveProject';
+
+describe("ActiveProject", () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      title: "",
+      navigation: {
+        navigate: jest.fn(),
+      },
+      contractors: [],
+      description: "",
+      photo: "",
+    }
+    wrapper = shallow(<ActiveProject props={props}/>)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
