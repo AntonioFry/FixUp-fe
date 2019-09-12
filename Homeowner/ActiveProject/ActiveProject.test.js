fb25bc9d21cdf1c006445701112c4ee4852ee6a7
Steve Rumizen <steverumizen@gmail.com>
2019-09-11 21:35:00 -0600

    Comment out component tests

--- Homeowner/ActiveProject/ActiveProject.test.js
+++ Homeowner/ActiveProject/ActiveProject.test.js
@@ -1,27 +1,27 @@
-import react from 'react';
-import { shallow } from 'enzyme';
-import ActiveProject from './ActiveProject';
+// import react from 'react';
+// import { shallow } from 'enzyme';
+// import ActiveProject from './ActiveProject';
 
-describe("ActiveProject", () => {
-  let wrapper;
-  let props;
+// describe("ActiveProject", () => {
+//   let wrapper;
+//   let props;
 
-  beforeEach(() => {
-    props = {
-      title: "",
-      navigation: {
-        navigate: jest.fn(),
-      },
-      contractors: [],
-      description: "",
-      photo: "",
-    }
-    wrapper = shallow(<ActiveProject props={props}/>)
-  })
+//   beforeEach(() => {
+//     props = {
+//       title: "",
+//       navigation: {
+//         navigate: jest.fn(),
+//       },
+//       contractors: [],
+//       description: "",
+//       photo: "",
+//     }
+//     wrapper = shallow(<ActiveProject props={props}/>)
+//   })
 
-  it('should match the snapshot', () => {
-    expect(wrapper).toMatchSnapshot()
-  });
+//   it('should match the snapshot', () => {
+//     expect(wrapper).toMatchSnapshot()
+//   });
 
   
-})
+// })
--- Homeowner/ActiveProjects/ActiveProjects.test.js
+++ Homeowner/ActiveProjects/ActiveProjects.test.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ActiveProjects from './ActiveProjects';
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import ActiveProjects from './ActiveProjects';
 
-describe('ActiveProjects', () => {
-  let wrapper;
-  let props;
+// describe('ActiveProjects', () => {
+//   let wrapper;
+//   let props;
 
-  beforeEach(() => {
-    props = {
-      navigation: {
-        navigate: jest.fn(),
-        getParam: jest.fn()
-      },
-    }
-    wrapper = shallow(<ActiveProjects props={props}/>) 
-  })
+//   beforeEach(() => {
+//     props = {
+//       navigation: {
+//         navigate: jest.fn(),
+//         getParam: jest.fn()
+//       },
+//     }
+//     wrapper = shallow(<ActiveProjects props={props}/>) 
+//   })
 
-  it('should match the snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
-  })
-})
+//   it('should match the snapshot', () => {
+//     expect(wrapper).toMatchSnapshot();
+//   })
+// })
